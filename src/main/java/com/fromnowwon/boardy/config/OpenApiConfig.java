439e06ddd9c631ea0f699a5878308b069501bd8a
Chaewon Jeong <fromnowwon@gmail.com>
2025-09-02 14:03:35 +0900

    feat: swagger 문서 설정

---
+++ src/main/java/com/fromnowwon/boardy/config/OpenApiConfig.java
@@ -0,0 +1,20 @@
+package com.fromnowwon.boardy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class OpenApiConfig {
+
+  @Bean
+  public OpenAPI boardyOpenAPI() {
+    return new OpenAPI()
+        .info(new Info()
+            .title("Boardy API")
+            .description("Boardy API 문서")
+            .version("v1.0"));
+  }
+}
