baaa1976ed2b29ea09a4c4a87ea6ca40cbaecd2a
Chaewon Jeong <fromnowwon@gmail.com>
2025-09-02 14:00:26 +0900

    first commit

---
+++ src/main/java/com/fromnowwon/boardy/BoardyApplication.java
@@ -0,0 +1,13 @@
+package com.fromnowwon.boardy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fromnowwon/boardy/BoardyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fromnowwon.boardy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
