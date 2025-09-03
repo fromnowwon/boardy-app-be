f864901fc4daf0e68b1dda46b1ee49c9e63733bf
Chaewon Jeong <fromnowwon@gmail.com>
2025-09-03 14:05:42 +0900

    feat: 로컬 환경 샘플 유저 및 관리자 데이터 초기화

---
+++ src/main/java/com/fromnowwon/boardy/config/DataInitializer.java
@@ -0,0 +1,47 @@
+package com.fromnowwon.boardy.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import com.fromnowwon.boardy.entity.User;
+import com.fromnowwon.boardy.enums.UserRole;
+import com.fromnowwon.boardy.repository.user.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@Profile("local") // local 프로파일에서만 실행
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public void run(String... args) {
+    if (userRepository.count() == 0) { // 중복 방지
+      // 일반 사용자
+      userRepository.save(User.builder()
+          .email("user1@example.com")
+          .password("user123")
+          .nickname("user1")
+          .role(UserRole.USER) // 기본 권한
+          .build());
+
+      userRepository.save(User.builder()
+          .email("user2@example.com")
+          .password("user123")
+          .nickname("user2")
+          .role(UserRole.USER)
+          .build());
+
+      // 관리자
+      userRepository.save(User.builder()
+          .email("admin@example.com")
+          .password("admin123")
+          .nickname("Admin")
+          .role(UserRole.ADMIN)
+          .build());
+    }
+  }
+}
