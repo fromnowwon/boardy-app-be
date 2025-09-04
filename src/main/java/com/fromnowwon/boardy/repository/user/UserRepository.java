c9d627b0969ae9fc259dc68c936fa0217c48c9ef
Chaewon Jeong <fromnowwon@gmail.com>
2025-09-04 17:14:50 +0900

    feat: 권한 데이터 접근 로직 생성

--- src/main/java/com/fromnowwon/boardy/repository/user/UserRepository.java
+++ src/main/java/com/fromnowwon/boardy/repository/user/UserRepository.java
@@ -5,13 +5,17 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fromnowwon.boardy.entity.User;
+import com.fromnowwon.boardy.enums.UserRole;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findByEmail(String email);
 
   Optional<User> findByNickname(String nickname);
 
+  Optional<User> findByRole(UserRole role);
+
   boolean existsByEmail(String email);
 
   boolean existsByNickname(String nickname);
+
 }
