2a0e39568b8fc9fa4a0e135ae944ea9875c4086f
Chaewon Jeong <fromnowwon@gmail.com>
2025-09-05 16:13:56 +0900

    chore: 에러 문구 수정 및 주석 추가

--- src/main/java/com/fromnowwon/boardy/service/auth/AuthService.java
+++ src/main/java/com/fromnowwon/boardy/service/auth/AuthService.java
@@ -15,6 +15,7 @@ public class AuthService {
   private final UserRepository userRepository;
   private final BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
+  // 회원가입
   public User signup(String email, String password, String nickname) {
     if (userRepository.findByEmail(email).isPresent()) {
       throw new RuntimeException("이미 존재하는 이메일입니다.");
@@ -32,11 +33,12 @@ public class AuthService {
     return userRepository.save(user);
   }
 
+  // 로그인
   public User login(String email, String password) {
     User user = userRepository.findByEmail(email)
-        .orElseThrow(() -> new RuntimeException("없는 이메일입니다."));
+        .orElseThrow(() -> new RuntimeException("존재하지 않는 이메일입니다."));
     if (!passwordEncoder.matches(password, user.getPassword())) {
-      throw new RuntimeException("비밀번호가 틀렸습니다.");
+      throw new RuntimeException("비밀번호가 맞지 않습니다.");
     }
     return user;
   }
